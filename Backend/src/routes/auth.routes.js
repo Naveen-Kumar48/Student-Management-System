e9082bd6ce3b8f8a959590aac3d282d73f69661f
Naveen Kumar <kumarnaveen897941@gmail.com>
2026-02-23 10:37:53 +0530

    implement user login feature with JWT authentication and protect middleware

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -1,8 +1,13 @@
 import express from "express";
-import { registerUser } from "../controllers/auth.controller.js";
+import { loginUser, registerUser } from "../controllers/auth.controller.js";
+import { protect } from "../middlewares/auth.middleware.js";
 
 
 const router = express.Router();
 router.post("/register", registerUser);
+router.post('/login',loginUser);
 
+router.get("/profile", protect,(req, res) => {
+  res.json(req.user);
+});
 export default router;
